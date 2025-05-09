01a268d80a57f425596aff58b6da667207206ba3
Magdalena Kajszczak <41114872+magdakajszczak@users.noreply.github.com>
2025-05-09 18:19:40 +0200

    Generate new component

---
+++ src/app/task-page/task-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-page',
+  imports: [],
+  templateUrl: './task-page.component.html',
+  styleUrl: './task-page.component.css'
+})
+export class TaskPageComponent {
+
+}
